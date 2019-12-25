9cc806a5c1c1daf80a8ee9e83e237b4aeb95daa8
susharm <susharm@adobe.com>
2019-12-25 14:47:59 +0530

    Messaging in poker rooms

---
+++ src/test/java/com/example/springchat/SpringMessagingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class SpringMessagingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
